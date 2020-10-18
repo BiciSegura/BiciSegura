b90bfe09d1048588c43e88fc1f13789f5d936e0e
DiegoRoomero <dromeroi@unal.edu.co>
2020-10-18 11:03:45 -0500

    (Prueba) Deberían guardarse los usuarios y el ingreso debería ser únicamente posible con un usuario registrado

--- app/src/main/java/com/pack/bicisegura/MainActivity.java
+++ app/src/main/java/com/pack/bicisegura/MainActivity.java
@@ -3,17 +3,27 @@ package com.pack.bicisegura;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+
 public class MainActivity extends AppCompatActivity {
+
+    LinkedList<Usuario> listausuarios;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loadData();
 
         Button registro = findViewById(R.id.registro);
 
@@ -33,12 +43,36 @@ public class MainActivity extends AppCompatActivity {
                 String Correo = ((EditText) findViewById(R.id.email)).getText().toString().trim();
                 String Pass = ((EditText) findViewById(R.id.password)).getText().toString().trim();
 
-                if(Correo.equals("1") && Pass.equals("2")){
+                Usuario us = new Usuario();
+
+                us.setCorreo(Correo);
+                us.setContraseña(Pass);
+
+                if(listausuarios.contain(us)){
                     Intent reg = new Intent(MainActivity.this, inicia_Pestañas.class);
                     startActivity(reg);
                 }
 
+                /*if(Correo.equals("1") && Pass.equals("2")){
+                    Intent reg = new Intent(MainActivity.this, inicia_Pestañas.class);
+                    startActivity(reg);
+                }*/
+
             }
         });
     }
+
+    private void loadData(){
+        SharedPreferences sharedPreferences = getSharedPreferences("shared preferences", MODE_PRIVATE);
+        Gson gson = new Gson();
+        String json = sharedPreferences.getString("task list", null);
+        Type type = new TypeToken<LinkedList<Usuario>>() {}.getType();
+        listausuarios = gson.fromJson(json, type);
+
+        if (listausuarios == null){
+            listausuarios = new LinkedList<Usuario>();
+        }
+
+    }
+
 }
--- app/src/main/java/com/pack/bicisegura/registrousuario.java
+++ app/src/main/java/com/pack/bicisegura/registrousuario.java
@@ -1,18 +1,30 @@
 package com.pack.bicisegura;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+
 public class registrousuario extends AppCompatActivity {
+
+    LinkedList<Usuario> listausuarios;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.registro);
+
+        loadData();
 
         Button registrarse = findViewById(R.id.registrarse);
 
@@ -21,11 +33,25 @@ public class registrousuario extends AppCompatActivity {
             public void onClick(View v) {
 
 
+
                 String correo = ((EditText) findViewById(R.id.correo)).getText().toString().trim();
                 String password = ((EditText) findViewById(R.id.contra)).getText().toString().trim();
                 String confpass = ((EditText) findViewById(R.id.confir_contra)).getText().toString().trim();
 
+                if(password.equals(confpass)){
 
+                    Usuario newuser = new Usuario();
+                    newuser.setContraseña(password);
+                    newuser.setCorreo(correo);
+
+                    listausuarios.insertLast(newuser);
+                    saveData();
+
+                }
+
+                else{
+                    Toast.makeText(registrousuario.this, "Por favor confirmar su contraseña correctamente", Toast.LENGTH_LONG).show();
+                }
 
                 //Próximamente?:
                 /*String Usuario = ((EditText) findViewById(R.id.usuario)).getText().toString().trim();
@@ -37,5 +63,29 @@ public class registrousuario extends AppCompatActivity {
                 //startActivity(reg);
             }
         });
+
+
     }
+    private void saveData(){
+        SharedPreferences sharedPreferences = getSharedPreferences("shared preferences", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        Gson gson = new Gson();
+        String json = gson.toJson(listausuarios);
+        editor.putString("task list", json);
+        editor.apply();
+    }
+
+    private void loadData(){
+        SharedPreferences sharedPreferences = getSharedPreferences("shared preferences", MODE_PRIVATE);
+        Gson gson = new Gson();
+        String json = sharedPreferences.getString("task list", null);
+        Type type = new TypeToken<LinkedList<Usuario>>() {}.getType();
+        listausuarios = gson.fromJson(json, type);
+
+        if (listausuarios == null){
+            listausuarios = new LinkedList<Usuario>();
+        }
+
+    }
+
 }
