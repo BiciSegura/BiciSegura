d3b830403f1f18194bff9749861b29e90bcbae06
Mateo Diaz <maadiazmu@unal.edu.co>
2020-11-01 17:00:08 -0500

    metodo altura

--- app/src/main/java/com/pack/bicisegura/Estructuras_arbol.java
+++ app/src/main/java/com/pack/bicisegura/Estructuras_arbol.java
@@ -1,34 +1,95 @@
-/*package com.pack.bicisegura;
+package com.pack.bicisegura;
 
-import java.lang.annotation.ElementType;
+class NodoAVL<T extends Comparable<?super T>> {
+    public T value;
+    public NodoAVL<T> left;
+    public NodoAVL<T> rigth;
+    public int heigth;
 
-public class Estructuras_Cola <E> extends LinkedList {
-
-    Node<E> head;
-    Node<E> tail;
-    int length;
-    public Estructuras_Cola(){
-        this.length = 0;
-        this.head = null;
-        this.tail = null;
+    public NodoAVL(T value) {
+        this.value = value;
+        this.left = null;
+        this.rigth = null;
+        this.heigth = -1;
     }
 
-    E poll (){
-        Node<E> T = new Node<E>();
-        T = (Node<E>) this.getLast();
-        this.deleteLast();
-        return (E) T;
-
+    public T getValue() {
+        return this.value;
     }
 
-    E peek (){
-        return (E) this.getLast();
+    public void setValue(T value) {
+        this.value = value;
     }
-
-    public void offer (E element){
-        this.insertFirst(element);
-    }
-
 
 }
-*/
+
+public class Estructuras_arbol<T extends Comparable<?super T>>{
+
+    public NodoAVL root;
+    int heigthleft=0;
+    int heigthrigth=0;
+
+    public int altura(NodoAVL root){
+
+        if(this.root != null){
+            if(this.root.left != null ){
+                heigthleft += 1;
+                altura(root.left);}
+            else if(this.root.rigth != null ){
+                heigthrigth += 1;
+                altura(root.left);}
+
+        }else{
+            if(heigthleft > heigthrigth){
+                root.heigth=heigthleft;
+            }else if (heigthleft == heigthrigth){
+                root.heigth=heigthleft;
+
+            }else {
+                root.heigth=heigthrigth;
+            }
+
+        }
+        return root.heigth;
+
+    }
+
+    /*public E datomenor(Estructura_arbol root){
+        E result=null;
+        while(root.value != null){
+            if(this.root.left.value > this.root.value && this.root.value < this.root.rigth.value ){
+                result = this.root.value;
+                return result;
+                break;
+            }
+            else if(this.root.left.value)
+
+
+
+
+        }
+        return result;
+
+    }
+    public E datomayor(Estructura_arbol root){
+        E result=null;
+        while(root.value != null){
+            if(this.root.left.value > this.root.value && this.root.value < this.root.rigth.value ){
+                result = this.root.value;
+                return result;
+                break;
+            }
+
+
+
+
+        }
+        return result;
+
+    }
+   public void insert(){}
+   public E findValue(){}
+
+
+}*/
+}
